4db3d8731929a4f057cf7972690a954d0930b2b8
Andrew S Erwin <andrew@erwininteractive.com>
2023-01-20 05:15:40 -0600

    rearrange some files

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,4 +1,9 @@
 <?php 
 
-require('paths.php');
 require(ROOT.'vendor/autoload.php');
+
+define('ROOT', dirname(__DIR__).'/');
+define('SRC', ROOT.'src/');
+define('DB', ROOT.'db/');
+define('VIEWS', ROOT.'app/views/');
+define('LAYOUTS', VIEWS.'layouts/');
