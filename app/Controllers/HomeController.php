fd10839ed9c0751858c980d1e6c62847be2850c7
Andrew S Erwin <andrewerwin73@gmail.com>
2023-09-20 14:20:48 -0500

    initial upload to gitlab

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Controllers;
 
@@ -9,20 +9,22 @@ use Framework\View;
 
 class HomeController extends Controller
 {
-	public function index(): View {
-        $data = [
-            'test' => "some text",
-            'cookie' => \Framework\App::init()->request()->cookie()
-        ];
+	public function index(): View
+	{
+		$data = [
+			'test' => "this is data originating from the Home controller.",
+		];
 
 		return $this->view('index', $data);
 	}
 
-	public function about(): View {
+	public function about(): View
+	{
 		return $this->view('about', ['about' => 'about text']);
 	}
 
-	public function contact(): View	{
+	public function contact(): View
+	{
 		return $this->view('contact');
 	}
 }
--- src/App.php
+++ src/App.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Framework;
 
@@ -11,29 +11,34 @@ class App
     private static App $instance;
     private Request $request;
 
-    public function __construct() {
+    public function __construct()
+    {
         self::$instance = $this;
-	}
-
-    public static function init(): App {
-        return self::$instance;  
     }
 
-    public function request(): Request {
-        return $this->request;
-    }
-
-    public static function get(): App {
+    public static function init(): App
+    {
         return self::$instance;
     }
 
-	public function run(Request $request): void {
+    public function request(): Request
+    {
+        return $this->request;
+    }
+
+    public static function get(): App
+    {
+        return self::$instance;
+    }
+
+    public function run(Request $request): void
+    {
         $this->request = $request;
 
-		try {
-			echo Router::dispatch($request);
-		} catch (Throwable $e) {
-			echo new View('_error', ['error' => $e]);
-		}
-	}
+        try {
+            echo Router::dispatch($request);
+        } catch (Throwable $e) {
+            echo new View('_error', ['error' => $e]);
+        }
+    }
 }
