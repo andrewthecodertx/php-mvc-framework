c1c657cb24bb05ad06892dad761f17b953d60224
Andrew S Erwin <andrewerwin73@gmail.com>
2024-01-22 23:38:37 -0600

    updating structure. improving docker config

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 
 session_start();
-
 require_once(__DIR__ . '/../bootstrap/init.php');
-
 
 if (!isset($_SESSION['USERNAME'])) {
   $_SESSION['USERNAME'] = \Faker\Factory::create()->firstName;
