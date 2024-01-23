5c4bc6654a5ffa9e52366ce718e7b1e32681bd4e
Andrew S Erwin <andrewerwin73@gmail.com>
2024-01-23 10:54:14 -0600

    update response object

--- src/Response.php
+++ src/Response.php
@@ -6,13 +6,67 @@ namespace Framework;
 
 class Response
 {
-    public function setStatus(int $code): void
-    {
-        http_response_code($code);
+  public function __construct(
+    private int $status = 200,
+    private array $headers = [],
+    private string $content = ''
+  ) {
+  }
+
+  public function setStatus(int $code): Response
+  {
+    http_response_code($code);
+    $this->status = $code;
+
+    return $this;
+  }
+
+  public function status(): int
+  {
+    return $this->status;
+  }
+
+  public function setContent($content): Response
+  {
+    $this->content = $content;
+
+    return $this;
+  }
+
+  public function content(): string
+  {
+    return $this->content;
+  }
+
+  public function addHeader(string $name, string $value): Response
+  {
+    $this->headers[$name] = $value;
+
+    return $this;
+  }
+
+  public function headers(): array
+  {
+    return $this->headers;
+  }
+
+  public function send(): void
+  {
+    http_response_code($this->status);
+
+    foreach ($this->headers as $name => $value) {
+      header("$name, $value");
     }
 
-    public function status(): int
-    {
-        return http_response_code();
-    }
+    echo $this->content;
+  }
+
+  public function redirect($url, $status = 302)
+  {
+    $this->setStatus($status);
+    $this->addHeader('Location', $url);
+    $this->send();
+
+    exit;
+  }
 }
--- src/Router.php
+++ src/Router.php
@@ -6,53 +6,57 @@ namespace Framework;
 
 use Exception;
 
-class Router {
-	private static array $routes = [];
+class Router
+{
+  private static array $routes = [];
 
-	public static function get(string $path, callable|array $callback): void {
-		$path = preg_replace('/\{([A-Za-z_]+)\}/', '(?<$1>[^/]+)', $path);
-		self::$routes['GET'][$path] = $callback;
-	}
+  public static function get(string $path, callable|array $callback): void
+  {
+    $path = preg_replace('/\{([A-Za-z_]+)\}/', '(?<$1>[^/]+)', $path);
+    self::$routes['GET'][$path] = $callback;
+  }
 
-	public static function post(string $path, callable|array $callback): void {
-		self::$routes['POST'][$path] = $callback;
-	}
+  public static function post(string $path, callable|array $callback): void
+  {
+    self::$routes['POST'][$path] = $callback;
+  }
 
-	public static function dispatch(Request $request): mixed {
-		$response = new Response;
-		$method = $request->method();
-		$path = $request->path();
+  public static function dispatch(Request $request): mixed
+  {
+    $response = new Response;
+    $method = $request->method();
+    $path = $request->path();
 
-		foreach (self::$routes[$method] as $route => $callback) {
-			$pattern = '#^' . $route . '$#';
-			$matches = [];
+    foreach (self::$routes[$method] as $route => $callback) {
+      $pattern = '#^' . $route . '$#';
+      $matches = [];
 
-			if (preg_match($pattern, $path, $matches)) {
-				$params = array_intersect_key($matches, array_flip(array_filter(array_keys($matches), 'is_string')));
+      if (preg_match($pattern, $path, $matches)) {
+        $params = array_intersect_key($matches, array_flip(array_filter(array_keys($matches), 'is_string')));
 
-				if (is_array($callback)) {
-					$callback[0] = new $callback[0];
-				}
+        if (is_array($callback)) {
+          $callback[0] = new $callback[0];
+        }
 
-				if (is_array($callback) && method_exists($callback[0], $callback[1])) {
-					return call_user_func_array([$callback[0], $callback[1]], [$request, $params]);
-				} else {
-					throw new Exception("LOOKING FOR A METHOD THAT DOESN'T EXIST!");
-				}
-			}
-		}
+        if (is_array($callback) && method_exists($callback[0], $callback[1])) {
+          return call_user_func_array([$callback[0], $callback[1]], [$request, $params]);
+        } else {
+          throw new Exception("LOOKING FOR A METHOD THAT DOESN'T EXIST!");
+        }
+      }
+    }
 
-		$callback = self::$routes[$method][$path];
+    $callback = self::$routes[$method][$path];
 
-		if (is_null($callback)) {
-			$response->setStatus(404);
-			throw new Exception("RESOURCE: $path NOT FOUND!", 404);
-		}
+    if (is_null($callback)) {
+      $response->setStatus(404);
+      throw new Exception("RESOURCE: $path NOT FOUND!", 404);
+    }
 
-		if (is_array($callback)) {
-			$callback[0] = new $callback[0];
-		}
+    if (is_array($callback)) {
+      $callback[0] = new $callback[0];
+    }
 
-		return call_user_func($callback);
-	}
+    return call_user_func($callback);
+  }
 }
